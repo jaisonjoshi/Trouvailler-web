47c2fd05180816d6c580460b5ee3b6832275107a
IRJ <jirenerose@gmail.com>
2022-10-13 19:12:39 +0530

    added custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'evergreen':'#2AF599',
+      'midnight':'#303030',
+    },
     extend: {},
   },
   plugins: [],
