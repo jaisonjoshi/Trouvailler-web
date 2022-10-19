e28b89e9ad90201d816c821150ee838e0921638d
IRJ <jirenerose@gmail.com>
2022-10-19 19:44:46 +0530

    made footer responsive

--- src/Pages/components/Footer/Footer.jsx
+++ src/Pages/components/Footer/Footer.jsx
@@ -16,6 +16,7 @@ const Footer = () => {
             <div><h5 className='text-sm p-4'>Page title</h5></div>
 
             <div className='hidden md:block md:row-span-3 md:pl-4'>
+              <h3 className='text-lg'>Subscribe to Newsletter</h3>
               <h5 className='text-sm pb-3'>Your email</h5>
               <div className='flex overflow-hidden h-11 rounded-lg w-full'>
                 <div><input type="text" className='h-full text-blacky-dark p-3 focus:outline-evergreen' /></div>
@@ -28,10 +29,18 @@ const Footer = () => {
             <div><h5 className='text-sm p-4'>Page title</h5></div>
             <div><h5 className='text-sm p-4'>Page title</h5></div>
           </div>
-        </div>  
+        </div>
+        <div className='md:hidden text-whiteglow p-4'>
+          <h3 className='text-lg'>Subscribe to Newsletter</h3>
+          <h5 className='text-sm pb-3'>Your email</h5>
+          <div className='flex overflow-hidden h-11 rounded-lg w-full'>
+            <div><input type="text" className='h-full grow text-blacky-dark p-3 focus:outline-evergreen' /></div>
+            <button className='flex-none bg-evergreen rounded-r-lg h-auto'><FontAwesomeIcon className='px-4' icon={faPaperPlane} /></button>
+          </div>
+        </div>
 
-        <div className='border-t-2 border-graydust-medium flex justify-between align-middle p-6 mx-10'>
-          <a href="" className='w-2/12'>
+        <div className='border-t-2 border-graydust-medium flex justify-between align-middle  p-6  mx-4 sm:mx-10'>
+          <a href="" className='w-4/12 sm:w-2/12'>
             <img src={Logo} alt="Trouvailler" />
           </a>
           <div className='flex justify-evenly'>
