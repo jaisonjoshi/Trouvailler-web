99961f7ee41f26a587e124a5c1101988f464109e
Irene Rose Johnson <jirenerose@gmail.com>
2022-11-06 23:32:16 +0530

    created hotel detail view

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from "./Pages/home/Home.jsx";
 import IntroToBid from "./Pages/Bid/IntroBid.jsx";
 import BidStatus from "./Pages/Bid/BidStatus.jsx";
 import Package from "./Pages/DetailViews/Package.jsx";
+import Hotel from "./Pages/DetailViews/Hotel.jsx"
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/what-is-bid" element={<IntroToBid/>}/>
         <Route path="/bid-status" element={<BidStatus/>}/>
         <Route path="/package" element={<Package/>}/>
+        <Route path="/hotel" element={<Hotel/>}/>
       </Routes>
     </BrowserRouter>
   );
