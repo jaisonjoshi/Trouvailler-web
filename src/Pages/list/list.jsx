bf4ef822b6d700ac986fd656c51c3ba795e3b571
HKS-30 <hemakshaji777@gmail.com>
2022-11-13 15:43:55 +0530

    Added items in list page

--- src/Pages/list/list.jsx
+++ src/Pages/list/list.jsx
@@ -9,10 +9,37 @@ const Home = () => {
     return (
         <div className="">
             <Navbar />
-            <div className="">
-
+            <div className="md:flex justify-center">
+                <div className="flex justify-center items-center m-3">
+                    <input type="text" className="w-48 flex justify-center outline-none items-center text-blacky-light p-3 shadow-lg shadow-gray-500 rounded-2xl text-xs" placeholder="Search"/>
+                </div>
+                <div className="flex justify-center items-center m-3">
+                        <button className="w-48 flex justify-center items-center text-blacky-light p-3 shadow-lg shadow-gray-500 rounded-2xl text-xs">
+                            <img src={require('../Assets/Variant2.png')} alt="Facebook" className="h-3 w-3 mr-2" />
+                            Categories
+                        </button>
+                    </div>
+                    <div className="flex justify-center items-center m-3">
+                        <button className="w-48 flex justify-center items-center text-blacky-light p-3 shadow-lg shadow-gray-500 rounded-2xl text-xs">
+                            <img src={require('../Assets/Variant2.png')} alt="Facebook" className="h-3 w-3 mr-2" />
+                            Budget
+                        </button>
+                    </div>
+                    <div className="flex justify-center items-center m-3">
+                        <button className="w-48 flex justify-center items-center text-blacky-light p-3 shadow-lg shadow-gray-500 rounded-2xl text-xs">
+                            <img src={require('../Assets/Variant2.png')} alt="Facebook" className="h-3 w-3 mr-2" />
+                            Activities
+                        </button>
+                    </div>
+                    <div className="flex justify-center items-center m-3">
+                        <button className="w-48 flex justify-center items-center text-blacky-light p-3 shadow-lg shadow-gray-500 rounded-2xl text-xs">
+                            <img src={require('../Assets/Variant2.png')} alt="Facebook" className="h-3 w-3 mr-2" />
+                            Rooms
+                        </button>
+                    </div>
             </div>
-            <div className="grid  sm:grid-cols-2 md:grid-cols-3 gap-5 m-11">
+            <hr className="w-full my-5" />
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5 m-11">
                 <List1 />
                 <List2 />
                 <List1 />
