d4af8018633015d213b08c2c94c9312f0253872f
Irene Rose Johnson <jirenerose@gmail.com>
2022-11-15 17:38:15 +0530

    Routed cards

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ function App() {
         <Route path="/list" element={<List/>}/>
         <Route path="/what-is-bid" element={<IntroToBid/>}/>
         <Route path="/bid-status" element={<BidStatus/>}/>
-        <Route path="/package" element={<Package/>}/>
-        <Route path="/hotel" element={<Hotel/>}/>
+        <Route path="/list/package" element={<Package/>}/>
+        <Route path="/list/hotel" element={<Hotel/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/components/cards/list2.jsx
+++ src/Pages/components/cards/list2.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const list2_card = () => {
   return (
-    <div className='overflow-hidden w-full relative'>
+    <Link to="/list/hotel">
+      <div className='overflow-hidden w-full relative'>
         <img className='h-60 sm:h-72 object-cover w-full rounded-lg' src={require('../../Assets/List.png')} alt="" />
         <div className='p-5'>
-            <h3 className='text-xl font-bold text-blacky-medium'>St. Louis Sanatorium</h3>
-            <p className='text-sm text-justify text-blacky-light'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some</p>    
+          <h3 className='text-xl font-bold text-blacky-medium'>St. Louis Sanatorium</h3>
+          <p className='text-sm text-justify text-blacky-light'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some</p>
         </div>
         <div className="p-5 flex justify-between">
-            <p className="text-evergreen text-xl font-bold">₹1,600.00</p>
-            <button className="bg-evergreen text-blacky-light font-semibold rounded-md w-32 h-10">Whats app</button>
+          <p className="text-evergreen text-xl font-bold">₹1,600.00</p>
+          <button className="bg-evergreen text-blacky-light font-semibold rounded-md w-32 h-10">Whats app</button>
         </div>
-    </div>
+      </div>
+    </Link>
   )
 }
 
